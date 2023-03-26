747673a59ecb410f94f39558553722cc30e81229
Angelo Rosso <angelo.rosso.19@cnu.edu>
2023-03-26 13:18:17 -0400

    Updates

--- batch/GenParticles.C
+++ batch/GenParticles.C
@@ -83,7 +83,7 @@ void GenParticles( int fPDGCode = 13, int nevents = 100,
 
   // Initialize input
   TString inname;
-  inname.Form("~/CDetOptical/macros/gep_12Gev1000.root");
+  inname.Form("~/CDetOptical/macros/gep_12Gev1mil.root");
   fInFileName = inname;
   InitInput();
   
